cef706f94d05ffa3e954ef8048a82f975b74ee0b
sumek20 <47901104+sumek20@users.noreply.github.com>
2023-03-07 08:16:15 -0500

    Add files via upload

---
+++ web.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state['new_todo'] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+    st.session_state['new_todo'] = ''
+
+st.title('My To-Do App')
+st.subheader("Type a Todo in the list below")
+
+st.text_input(label='', placeholder='Add New ToDo...',
+              on_change=add_todo, key='new_todo')
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+
+
